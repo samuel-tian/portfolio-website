cae4cfb057114b3699448ed4756e97e6612606cd
Samuel Tian <swtian@mit.edu>
2022-11-18 01:23:45 -0500

    fiat crypto description update

--- pages/fiatcrypto.jsx
+++ pages/fiatcrypto.jsx
@@ -1,35 +1,76 @@
 import React from "react";
 import { AiFillGithub } from "react-icons/ai";
+import { IoIosPaper } from "react-icons/io";
 
 const fiatcrypto = () => {
   return (
     <div className="w-full">
       <h2>Fiat Cryptography</h2>
       <div className="flex gap-2">
-        <div className='tag'>Formal Verification</div>
-        <div className='tag'>Coq</div>
+        <div className="tag">Formal Verification</div>
+        <div className="tag">Coq</div>
       </div>
-      <a href="https://github.com/mit-plv/fiat-crypto" target="_blank" rel="noreferrer">
-        <div className="inline-block">
-          <div className="btn">
-            <AiFillGithub />
+      <div className="flex gap-2">
+        <a
+          href="https://github.com/mit-plv/fiat-crypto"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <div className="inline-block">
+            <div className="btn">
+              <AiFillGithub />
+            </div>
           </div>
-        </div>
-      </a>
+        </a>
+        <a
+          href="https://cs.adelaide.edu.au/~yval/pdfs/KuepperEGSWCCGWY23.pdf"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <div className="inline-block">
+            <div className="btn">
+              <IoIosPaper />
+            </div>
+          </div>
+        </a>
+      </div>
 
       <p>
         fiat-crypto is a library of cryptographic arithmetic implementations,
         compiled from short, high-level code templates. The templates are proven
         correct using the interactive theorem prover Coq, and a verified
-        compiler generates low-level code that is competitive with manually
-        optimized assembly.
+        compiler generates low-level code in languages such as C, Golang, and
+        even JSON.
       </p>
       <p>
-        I implemented and proved functional correctness of the Solinas reduction
-        algorithm, which is necessary for implementations of Curve25519 and
-        other popular elliptic curves. Recent developments in the fiat-crypto
-        compiler infrastructure would enable this algorithm to be faster than
-        the library&apos;s existing reduction algorithm.
+        I implemented and proved functional correctness of saturated field
+        arithmetic algorithms, including Solinas reduction and squaring, which
+        is necessary for implementations of Curve25519-based encryption schemes.
+        Previously, the library only supported unsaturated arithmetic in this
+        prime field due to a limitation in the bounds inference pipeline, but
+        through some clever workarounds my template was able to synthesize
+        succesfully! My contributions can be viewed in fiat-crypto&apos;s open
+        source repository on Github, linked above.
+      </p>
+      <p>
+        We also collaborated with researchers from the University of Adelaide
+        who developed CryptOpt, a compilation pipeline that takes the output of
+        the fiat-crypto pipeline as input. CryptOpt compiles high-level
+        functional implementations of the arithmetic algorithms down to
+        assembly, producing output that is significantly faster than gcc/clang
+        output. The platform then applies genetic improvement, which makes
+        slight random changes in the assembly to improve runtime. With these
+        strategies, CryptOpt is able to optimize my template to outperform
+        industry-standard OpenSSL implementations of Curve25519 field arithmetic
+        on newer Intel machines! The OpenSSL implementations are manually
+        written and optimized, while generation of assembly produced from the
+        fiat-crypto + CryptOpt pipeline is automated and comes with the
+        additional benefit of formal proofs of correctness.
+      </p>
+      <p>
+        More information about CryptOpt and fiat-crypto can be found in the
+        paper linked above. The paper has been submitted to the 2023 Conference
+        on Programming Language Design and Implementation (PLDI).
       </p>
     </div>
   );
--- pages/surgeryrl.jsx
+++ pages/surgeryrl.jsx
@@ -6,11 +6,15 @@ const surgeryrl = () => {
     <div className="w-full">
       <h2>Surgical Reinforcement Learning</h2>
       <div className="flex gap-2">
-        <div className='tag'>Reinforcement Learning</div>
-        <div className='tag'>Pytorch</div>
-        <div className='tag'>Python</div>
+        <div className="tag">Reinforcement Learning</div>
+        <div className="tag">Pytorch</div>
+        <div className="tag">Python</div>
       </div>
-      <a href="https://github.com/samuel-tian/IsaacGymEnvs" target="_blank" rel="noreferrer">
+      <a
+        href="https://github.com/samuel-tian/IsaacGymEnvs"
+        target="_blank"
+        rel="noreferrer"
+      >
         <div className="inline-block">
           <div className="btn">
             <AiFillGithub />
@@ -21,7 +25,7 @@ const surgeryrl = () => {
         Laparoscopic cholecystectomy is a minimally invasive surgical removal of
         the gallbladder. Multiple small incisions are made in the abdomen of the
         patient, and several tools along with a video camera are inserted to
-        perform the surgery via a control system.
+        perform the surgery via a robotic control system.
       </p>
       <p>
         Currently, research related to reinforcement learning applications in
@@ -29,6 +33,11 @@ const surgeryrl = () => {
         the risk of such surgeries), so I am working on designing a RL
         environment that simulates a patient side manipulator interacting with a
         deformable liver-gallbladder compound mesh.
+      </p>
+      <p>
+        Note that the Github repository above is out of date, as I had to make
+        several of the mesh asset files that we are using private. Sorry for the
+        inconvenience!
       </p>
     </div>
   );
